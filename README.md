89a2e243004ca70f3a407118c1d18c3c554702f5
hipro <luanshome@gmail.com>
2013-12-19 21:07:47 +0800

    Optimized

--- README.md
+++ README.md
@@ -12,6 +12,16 @@ Generate epub for biquge.com .
 > cd bigugeepub  
 > python biqugeepub.py 小说名称
 
+如果小说内容较多（比如2000多章），需要的时间会长一些。偶尔会由于网络、网站原因而中断。
+
 ### 用epub阅读器阅读
 
 OS X下可以用iBook打开，检查epub是否可用。Windows可以下载[Adobe的epub阅读器](http://www.adobe.com/products/digital-editions/download.html)。PC、手机、Kindle上可以使用[Kindle阅读软件](http://www.amazon.cn/gp/feature.html/ref=sa_menu_kindle_l2_f98968?ie=UTF8&docId=98968)。锦书BamBook可以通过[云梯软件](http://dlc2.sdo.com/FTP/ebook/20111010/1/snda_PCClient_0.98.zip)上传。等等……(´･_･`)
+
+### 用iBook打开后的一些截图
+
+目录   
+![epub-preview 1](https://lh4.googleusercontent.com/-ygB-eQSGAEM/UrLqAWawCaI/AAAAAAAAAB4/d_CYZc5SjoA/s800/epub2.jpg "目录")
+
+内容   
+![epub-preview 2](https://lh3.googleusercontent.com/-2cOi4r9lOsg/UrLqBMuk85I/AAAAAAAAACE/D-apzvJXJNw/s800/epub4.jpg "内容")
--- biqugeepub.py
+++ biqugeepub.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'hipro'
-__version__ = '2013.12.13'
+__version__ = '2013.12.19'
 __license__ = 'GPL'
 
 from sys import argv, exit as esc
-from urllib2 import urlopen, Request#, quote
+from urllib2 import urlopen, Request
 from re import findall,sub,search, U, S
 from os import chdir,walk,remove
 from os.path import join,exists
@@ -31,12 +31,18 @@ class BiqugeEpub(object):
         user_agent='Chrome/32.0.1667.0 Safari/537.36'
         req.add_header('User-agent', user_agent)
         # get response
-        resp=urlopen(req,None,TIMEOUT)
+        try:
+            resp=urlopen(req,None,TIMEOUT)
+        except Exception as e:
+            if resp:
+                print resp.code, resp.msg
+            else: 
+                print e
         # status code 200 - 'ok'.
         if resp.code==200:
             return resp.read()
         else:
-            return resp.msg
+            return ""
        
     def query_book_info(self):
         """docstring for query_book_id"""
@@ -77,6 +83,61 @@ class BiqugeEpub(object):
         try:
             book_info=self.query_book_info()
             
+            # open book site, get book info.
+            html=self.open_url(book_info['bookurl']).decode('gb18030').encode('utf-8')
+            if len(html)>100:
+                print "===Retrieving book information."
+            subject=search('<a href="/[a-z]{4,10}xiaoshuo/">(.+?)小说</a>', html, U)
+            if subject:
+                book_info['subject']=subject.group(1)
+            print "Subject :", book_info.get('subject')
+            
+            author=search('<p>作.*?者：(.+?)</p>', html, U)
+            if author:
+                book_info['author']=author.group(1)
+                #book_info['authorurl']=book_info['authorurl'] % book_info['author']
+            print "Author :", book_info['author']
+            #print "Authorurl is", book_info.get('authorurl')
+                  
+            description=search('<div id="intro">\\s+<p>(.+?)</p>.+?</div>', html, U|S)
+            if description:
+                book_info['description']=description.group(1).strip().replace(' ','').replace('&nbsp;','').replace('<br>','\n')
+            print "Description :", book_info.get('description')
+            
+            datetime=search('<p>最后更新：(.+?)</p>', html)
+            if datetime:
+                book_info['datetime']=datetime.group(1).strip()
+            print "Last update :", book_info.get('datetime')
+            
+            title_all=findall('<a href="/[0-9]{1,2}_[0-9]{1,9}/([0-9]{1,9})\.html">(.+?)</a>', html, U)
+            del html
+            
+            # Remove the cache 9 chapters.
+            if len(title_all)>9: del title_all[:9]
+            print 'Total Chapters:', len(title_all)
+            
+            # create foundation documents.
+            # copy template
+            if exists(self.book_id): 
+                rmtree(self.book_id)
+                print "===Remove old files."
+                sleep(3)
+            
+            print "===Creating the foundation documents."                    
+            copytree('epub_template',self.book_id)
+            chdir(self.book_id)
+            
+            epub_path='../%s.epub' % self.book_name
+            if exists(epub_path): remove(epub_path)
+                      
+            content_html=file('content.html', 'r')
+            temp_con=content_html.read()
+            content_html.close()            
+            remove('content.html')
+            
+            no=3 # playOrder for ncx.
+            base_url='http://www.biquge.com/%(bookidf)s_%(bookid)s/content.html' % {'bookidf':self.book_id_f,'bookid':book_info['bookid']}
+
             render_for={
                         'itemlist':['<item id="%(contentid)s" href="%(contentid)s.html" media-type="application/xhtml+xml" />',],
                         'itemreflist':['<itemref idref="%(contentid)s" />',],
@@ -84,77 +145,6 @@ class BiqugeEpub(object):
                         'titlelist':['<li%(even_class_flag)s><a href="%(contentid)s.html">%(title)s</a></li>',],
                         }
             
-            # open book site
-            html=self.open_url(book_info['bookurl']).decode('gb18030').encode('utf-8')
-            if len(html)>100:
-                print "===Retrieving book information."
-            subject=search('<a href="/[a-z]{4,10}xiaoshuo/">(.+?)小说</a>', html, U)
-            if subject:
-                book_info['subject']=subject.group(1)
-            print "subject :", book_info.get('subject')
-            
-            author=search('<p>作.*?者：(.+?)</p>', html, U)
-            if author:
-                book_info['author']=author.group(1)
-                #book_info['authorurl']=book_info['authorurl'] % book_info['author']
-            print "['author :", book_info['author']
-            #print "book_info['authorurl'] is", book_info.get('authorurl')
-                  
-            description=search('<div id="intro">\\s+<p>(.+?)</p>.+?</div>', html, U|S)
-            if description:
-                book_info['description']=description.group(1).strip().replace(' ','').replace('&nbsp;','').replace('<br>','\n')#.replace(u'　','').replace(' ','')
-            print "description :", book_info.get('description')
-            datetime=search('<p>最后更新：(.+?)</p>', html)
-            if datetime:
-                book_info['datetime']=datetime.group(1).strip()
-            print "last update :", book_info.get('datetime')
-            
-            title_all=findall('<a href="/[0-9]{1,2}_[0-9]{1,9}/([0-9]{1,9})\.html">(.+?)</a>', html, U)
-            del html
-            
-            # Remove the cache 9 chapters.
-            if len(title_all)>9: title_all=title_all[9:]
-            
-            # copy template
-            if exists(self.book_id): 
-                rmtree(self.book_id)
-                print "===Remove old files."
-                sleep(3)
-                                
-            copytree('epub_template',self.book_id)
-            chdir(self.book_id)
-            
-            epub_path='../%s.epub' % self.book_name
-            if exists(epub_path): remove(epub_path)
-            
-            # create 
-                    
-            # catalog.html
-            cat_t=file('catalog.html','r+')
-            cat_str=cat_t.read()
-            cat_str=cat_str.replace('\r',"")
-            cat_t.seek(0)
-            
-            # toc.ncx
-            toc_t=file('toc.ncx','r+')
-            toc_str=toc_t.read()
-            toc_t.seek(0)
-            
-            # content.opf
-            con_t=file('content.opf','r+')
-            con_str=con_t.read()
-            con_t.seek(0)
-            
-            def read_file(path,mode='r'):
-                f=file(path,mode)
-                content=f.read()
-                f.close()
-                return content
-        
-            no=3 # playOrder for ncx.
-            base_url='http://www.biquge.com/%(bookidf)s_%(bookid)s/content.html' % {'bookidf':self.book_id_f,'bookid':book_info['bookid']}
-    
-            print "===Creating the foundation documents."
             for title in title_all:
                 
                 html=self.open_url(base_url.replace('content',title[0])).decode('gb18030').encode('utf-8')
@@ -165,7 +155,7 @@ class BiqugeEpub(object):
                     content=sub("(href=)(http://.+?)([\\b|>])",lambda m: "".join([m.group(1),'"',m.group(2),'"',m.group(3)]),content)
                 content="".join(["<p>",content,"</p>"])
                 
-                w_c=read_file(r'content.html').replace("{{title}}",title[1]).replace("{{content}}",content)
+                w_c=temp_con.replace("{{title}}",title[1]).replace("{{content}}",content)
                 
                 contentid="content%s_%s" % (self.book_id, title[0])
                 f=file('.'.join([contentid,'html']),'w')# create content.html
@@ -181,27 +171,13 @@ class BiqugeEpub(object):
                 render_for['titlelist'].append(render_for['titlelist'][0] % {'contentid':contentid, 'title':title[1], 'even_class_flag':even_class_flag})
                 
             del title_all
-            remove('content.html')
+            del temp_con
             
             # add page site.
             no+=1
             even_class_flag=('',' class="even"')[(no-3)%2]
             render_for['titlelist'].append(render_for['titlelist'][0].replace('html','xhtml') % {'contentid':'page', 'title':'关于%s' % book_info['sitenameabbr'], 'even_class_flag':even_class_flag})
             render_for['navPointlist'].append(render_for['navPointlist'][0].replace('html','xhtml') % {'contentid':'page', 'title':'关于%s' % book_info['sitenameabbr'], 'no':str(no)})
-            
-            
-            cat_str=cat_str.replace('{{for_titlelist}}',"\n".join(render_for['titlelist'][1:]))
-            toc_str=toc_str.replace('{{for_navPointlist}}',"".join(render_for['navPointlist'][1:]))
-            con_str=con_str.replace('{{for_itemlist}}',"".join(render_for['itemlist'][1:]))
-            con_str=con_str.replace('{{for_itemreflist}}',"".join(render_for['itemreflist'][1:]))
-            
-            del render_for
-            
-            # title.xhtml
-            tit_t=file('title.xhtml','r+')
-            tit_str=tit_t.read()
-            tit_str=tit_str.replace('\r',"")
-            tit_t.seek(0)
             
             def render(f_str):
                 if '%' in f_str:
@@ -211,45 +187,72 @@ class BiqugeEpub(object):
                 if '%%' in f_str:
                     f_str=f_str.replace('%%','%')
                 return f_str
-                
             
-            tit_t.write(render(tit_str))
-            tit_t.close()
-            cat_t.write(render(cat_str))
-            cat_t.close()
-            toc_t.write(render(toc_str))
-            toc_t.close()
-            con_t.write(render(con_str))
-            con_t.close()
+            # catalog.html, toc.ncx, content.opf, title.xhtml
+            with file('catalog.html','r+') as cat_t, file('toc.ncx','r+') as toc_t, \
+                file('content.opf','r+') as con_t, file('title.xhtml','r+') as tit_t :
+                
+                tit_str=tit_t.read()
+                tit_str=tit_str.replace('\r',"")
+                tit_t.seek(0)
+                tit_t.write(render(tit_str))
+                del tit_str
+                
+                cat_str=cat_t.read()
+                cat_str=cat_str.replace('\r',"")
+                cat_t.seek(0)
+                
+                toc_str=toc_t.read()
+                toc_t.seek(0)
+                
+                con_str=con_t.read()
+                con_t.seek(0)
+                
+                cat_str=cat_str.replace('{{for_titlelist}}',"\n".join(render_for['titlelist'][1:]))
+                cat_t.write(render(cat_str))
+                del cat_str
+                toc_str=toc_str.replace('{{for_navPointlist}}',"".join(render_for['navPointlist'][1:]))
+                toc_t.write(render(toc_str))
+                del toc_str
+                con_str=con_str.replace('{{for_itemlist}}',"".join(render_for['itemlist'][1:]))
+                con_str=con_str.replace('{{for_itemreflist}}',"".join(render_for['itemreflist'][1:]))
+                con_t.write(render(con_str))
+                del con_str
+                
+            del render_for
             
             # get cover.jpg 
             jpg=file('cover.jpg','wb')
             jpg.write(self.open_url(book_info['img_url']))
             jpg.close()
+            
             print '===Being generated.'
+            # zip *.epub
             zip=ZipFile(epub_path,'w',ZIP_DEFLATED)
             for b,ds,fs in walk('.'):
                 for ff in fs:
                     zip.write(join(b,ff))
             zip.close()
             
+            print "===Cleaned temp files."
             chdir("../.") 
             rmtree(self.book_id)
-            print "===Cleaned temp files."
             sleep(3)
             
             print '===Finish!'
             print "Epub file path is ./%s.epub." % self.book_name
+            
         except Exception as e:
             #print e
-            print "Test whether you can read this novel on <biqugecom.com> with your browser. If it's okay, you can re-run program again or feedback this issue to <https://github.com/hipro/BiqugeEpub.git>."
+            print "Fail!"
+            print "Test whether you can read this novel on <biqugecom.com> with your browser. If it's okay, you can re-run program again or feedback this issue to < https://github.com/hipro/BiqugeEpub >."
 
 def main():   
     epub=BiqugeEpub(argv[1])
     epub.generate_epub()
 
 # def test():
-#     epub=BiqugeEpub("末世盗贼行")#create a BiqugeEpub class
+#     epub=BiqugeEpub("凡人修仙传")#create a BiqugeEpub class
 #     #print epub.query_book_info()
 #     epub.generate_epub()
 # test()
